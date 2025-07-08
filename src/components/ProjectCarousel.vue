8f5fb220c3f2ae2e9aa5b2a97b572767f1fcd35e
unknown <andreivilla63@gmail.com>
2025-07-08 19:26:06 -0300

    correção dos botões e layout mobile

--- src/components/ProjectCarousel.vue
+++ src/components/ProjectCarousel.vue
@@ -10,8 +10,17 @@
     </ul>
 
     <div class="nav-buttons">
-      <CircleBtn @click="prev">⮜</CircleBtn>
-      <CircleBtn @click="next">⮞</CircleBtn>
+      <CircleBtn @click="prev">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+          <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+        </svg>
+      </CircleBtn>
+      
+      <CircleBtn @click="next">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+        </svg>
+      </CircleBtn>
     </div>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/portifolio-vue/', // nome do repositório
 })
